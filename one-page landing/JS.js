2fb313762e0bf52ff7c9e633a1cffaa3fd05defa
MohammadAlwiendar <126337564+MohammadAlwiendar@users.noreply.github.com>
2023-02-25 06:38:08 -0800

    Add files via upload

---
+++ one-page landing/JS.js
@@ -0,0 +1,11 @@
+function togglePopup1(){
+	document.getElementById("p1").classList.toggle("active");
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
